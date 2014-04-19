2b97cd79375a5d267e1b7d3d3335f664b118736d
Daiki Asahi <dalto.saxphone@gmail.com>
2014-04-20 01:12:26 +0900

    Remove AFNeworking reference from GPHTTPClient.m

--- GrowthPush/GPHttpClient.m
+++ GrowthPush/GPHttpClient.m
@@ -7,25 +7,21 @@
 //
 
 #import "GPHttpClient.h"
-#import "AFNetworking.h"
 
 static GPHttpClient *sharedInstance = nil;
 
 @interface GPHttpClient () {
 
-    AFHTTPClient *httpClient;
     NSURL *baseUrl;
 
 }
 
-@property (nonatomic, retain) AFHTTPClient *httpClient;
 @property (nonatomic, retain) NSURL *baseUrl;
 
 @end
 
 @implementation GPHttpClient
 
-@synthesize httpClient;
 @synthesize baseUrl;
 
 + (GPHttpClient *) sharedInstance {
@@ -38,7 +34,6 @@ static GPHttpClient *sharedInstance = nil;
 
 - (void) dealloc {
 
-    self.httpClient = nil;
     self.baseUrl = nil;
 
     [super dealloc];
@@ -48,16 +43,6 @@ static GPHttpClient *sharedInstance = nil;
 - (void) httpRequest:(GPHttpRequest *)httpRequest success:(void (^)(GPHttpResponse *httpResponse))success fail:(void (^)(GPHttpResponse *httpResponse))fail {
 
     NSURLRequest *urlRequest = [httpRequest urlRequestWithBaseUrl:baseUrl];
-
-    AFJSONRequestOperation *requestOperation = [AFJSONRequestOperation JSONRequestOperationWithRequest:urlRequest success:^(NSURLRequest *request, NSHTTPURLResponse *response, id JSON) {
-            if (success)
-                success([GPHttpResponse instanceWithUrlRequest:urlRequest httpUrlResponse:response error:nil body:JSON]);
-        } failure:^(NSURLRequest *request, NSHTTPURLResponse *response, NSError *error, id JSON) {
-            if (fail)
-                fail([GPHttpResponse instanceWithUrlRequest:urlRequest httpUrlResponse:response error:error body:JSON]);
-        }];
-
-    [requestOperation start];
 
 }
 
