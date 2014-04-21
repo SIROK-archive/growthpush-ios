660066183bcd5f843abebf27250b9d8cc41fa5fc
Naoyuki Kataoka <kataokanaoyuki@gmail.com>
2014-04-22 01:27:27 +0900

    Add sample project

---
+++ sample/GrowthPushSample/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  GrowthPushSample
+//
+//  Created by Kataoka Naoyuki on 2014/04/22.
+//  Copyright (c) 2014年 SIROK, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ sample/GrowthPushSampleTests/GrowthPushSampleTests.m
@@ -0,0 +1,34 @@
+//
+//  GrowthPushSampleTests.m
+//  GrowthPushSampleTests
+//
+//  Created by Kataoka Naoyuki on 2014/04/22.
+//  Copyright (c) 2014年 SIROK, Inc. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface GrowthPushSampleTests : XCTestCase
+
+@end
+
+@implementation GrowthPushSampleTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
